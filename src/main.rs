b28c693b0bbffa84c20aaa9a019ba4565a43dd3e
ar <arjun_patel1478@protonmail.com>
2023-10-18 03:04:13 -0500

    added the start to the api

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,99 @@
-
-use sqlx::{Connection,postgres::PgPoolOptions, Executor};
-use reqwest::{self, Error};
-use serde_json::{Value,Result};
-use chrono::{Utc, TimeZone, NaiveDateTime,DateTime};
-use tokio::time::{sleep, Duration};
+use std::collections::HashMap;
+use std::fmt::Debug;
 use std::thread;
+use sqlx::{Connection, postgres::PgPoolOptions, Executor, Row, FromRow, PgPool};
+use reqwest::{self};
+use serde_json::{Value};
+use chrono::{Utc, NaiveDateTime,DateTime};
+use chrono::format::Item;
+use sqlx::postgres::PgQueryResult;
+use tokio::time::{Duration};
+use warp::Filter;
+use serde::{Deserialize, Serialize};
+use tokio::runtime::Runtime;
+use tokio::task;
 
+
+#[derive(serde::Serialize, FromRow, Debug)]
+pub struct Items {
+    pub id: i32,
+    pub item_id: String,
+    pub is_del: bool,
+}
 #[tokio::main]
 async fn main(){
+    println!("Hello, world!");
     make_new_time_thing().await;
+    let tsk1 = async { scrape().await };
+    let tsk2 = async { wrp().await };
+    let tsk3 = async { wrp2().await };
+    tokio::join!(tsk1, tsk2, tsk3);
+}
+
+
+async fn wrp() {
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect("postgres://user:postgres123@localhost:5436/bazz")
+        .await
+        .unwrap();
+
+    let get_items_route = warp::path("items")
+        .and(warp::get())
+        .and(warp::any().map(move || pool.clone()))
+        .and_then(get_items);
+
+    warp::serve(get_items_route)
+        .run(([127, 0, 0, 1], 3030))
+        .await;
+
+
+}
+
+
+async fn wrp2() {
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect("postgres://user:postgres123@localhost:5436/bazz")
+        .await
+        .unwrap();
+
+    let get_items_route = warp::path("items")
+        .and(warp::get())
+        .and(warp::any().map(move || pool.clone()))
+        .and_then(get_items);
+    warp::serve(get_items_route)
+        .run(([10, 30, 173, 148], 3031))
+        .await;
+
+
+}
+
+
+async fn get_items(pool: PgPool) -> Result<impl warp::Reply, warp::Rejection> {
+    println!("get items");
+    let items = sqlx::query("SELECT id, item_id, is_delisted FROM items")
+        .fetch_all(&pool)
+        .await
+        .map_err(|e| {
+            eprintln!("Database error: {}", e);
+            warp::reject()
+        })?;
+    let items2: Vec<Items> = items
+        .into_iter()
+
+        .map(|row| Items {
+            id: row.get("id"),
+            item_id: row.get("item_id"),
+            is_del: row.get("is_delisted")
+        })
+        .collect();
+
+    Ok(warp::reply::json(&items2))
+}
+
+
+async fn scrape(){
     let mut it = 0;
     let start_time2 = std::time::Instant::now();
     loop {
@@ -26,12 +111,7 @@ async fn main(){
             None => continue,
         }
     }
-
 }
-
-
-
-
 
 async fn get_baz_data() -> std::string::String{
     let resp = reqwest::get("https://api.hypixel.net/skyblock/bazaar").await;
@@ -152,6 +232,50 @@ async fn add_sell_info() {
         }
     }
 
+async fn make_chart_data(){
+    one_min_data().await;
+    five_min_data().await;
+    thirty_min_data().await;
+    one_hour_data().await;
+    four_hour_data().await;
+    one_day_data().await;
+    one_week_data().await;
+    one_month_data().await;
+}
+
+async fn one_min_data(){
+
+}
+
+async fn five_min_data(){
+
+}
+
+async fn thirty_min_data(){
+
+}
+
+async fn one_hour_data(){
+
+}
+
+async fn four_hour_data(){
+
+}
+
+async fn one_day_data(){
+
+}
+
+async fn one_week_data(){
+
+}
+
+async fn one_month_data(){
+
+}
+
+
 
 async fn make_new_time_thing(){
     let pool = PgPoolOptions::new()
@@ -228,6 +352,126 @@ async fn make_new_time_thing(){
         );
     ")).await.expect("summery buy");
 
+    pool.execute(sqlx::query("
+        CREATE TABLE IF NOT EXISTS one_min_chart(
+            item_id TEXT NOT NULL,
+            time TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+            open_price FLOAT NOT NULL ,
+            close_price FLOAT NOT NULL ,
+            high_price FLOAT NOT NULL ,
+            low_price FLOAT NOT NULL ,
+            volume FLOAT NOT NULL ,
+            average_price FLOAT NOT NULL,
+            PRIMARY KEY (item_id, time),
+            CONSTRAINT fk_item FOREIGN KEY (item_id) REFERENCES items (item_id)
+        );
+    ")).await.expect("1 min chart");
+
+    pool.execute(sqlx::query("
+        CREATE TABLE IF NOT EXISTS five_min_chart(
+            item_id TEXT NOT NULL,
+            time TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+            open_price FLOAT NOT NULL ,
+            close_price FLOAT NOT NULL ,
+            high_price FLOAT NOT NULL ,
+            low_price FLOAT NOT NULL ,
+            volume FLOAT NOT NULL ,
+            average_price FLOAT NOT NULL,
+            PRIMARY KEY (item_id, time),
+            CONSTRAINT fk_item FOREIGN KEY (item_id) REFERENCES items (item_id)
+        );
+    ")).await.expect("5 min chart");
+
+    pool.execute(sqlx::query("
+        CREATE TABLE IF NOT EXISTS thirty_min_chart(
+            item_id TEXT NOT NULL,
+            time TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+            open_price FLOAT NOT NULL ,
+            close_price FLOAT NOT NULL ,
+            high_price FLOAT NOT NULL ,
+            low_price FLOAT NOT NULL ,
+            volume FLOAT NOT NULL ,
+            average_price FLOAT NOT NULL,
+            PRIMARY KEY (item_id, time),
+            CONSTRAINT fk_item FOREIGN KEY (item_id) REFERENCES items (item_id)
+        );
+    ")).await.expect("30 min chart");
+
+    pool.execute(sqlx::query("
+        CREATE TABLE IF NOT EXISTS one_hour_chart(
+            item_id TEXT NOT NULL,
+            time TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+            open_price FLOAT NOT NULL ,
+            close_price FLOAT NOT NULL ,
+            high_price FLOAT NOT NULL ,
+            low_price FLOAT NOT NULL ,
+            volume FLOAT NOT NULL ,
+            average_price FLOAT NOT NULL,
+            PRIMARY KEY (item_id, time),
+            CONSTRAINT fk_item FOREIGN KEY (item_id) REFERENCES items (item_id)
+        );
+    ")).await.expect("1 hour chart");
+
+    pool.execute(sqlx::query("
+        CREATE TABLE IF NOT EXISTS four_hour_chart(
+            item_id TEXT NOT NULL,
+            time TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+            open_price FLOAT NOT NULL ,
+            close_price FLOAT NOT NULL ,
+            high_price FLOAT NOT NULL ,
+            low_price FLOAT NOT NULL ,
+            volume FLOAT NOT NULL ,
+            average_price FLOAT NOT NULL,
+            PRIMARY KEY (item_id, time),
+            CONSTRAINT fk_item FOREIGN KEY (item_id) REFERENCES items (item_id)
+        );
+    ")).await.expect("4 hour chart");
+
+    pool.execute(sqlx::query("
+        CREATE TABLE IF NOT EXISTS one_day_chart(
+            item_id TEXT NOT NULL,
+            time TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+            open_price FLOAT NOT NULL ,
+            close_price FLOAT NOT NULL ,
+            high_price FLOAT NOT NULL ,
+            low_price FLOAT NOT NULL ,
+            volume FLOAT NOT NULL ,
+            average_price FLOAT NOT NULL,
+            PRIMARY KEY (item_id, time),
+            CONSTRAINT fk_item FOREIGN KEY (item_id) REFERENCES items (item_id)
+        );
+    ")).await.expect("1 day chart");
+
+    pool.execute(sqlx::query("
+        CREATE TABLE IF NOT EXISTS one_week_chart(
+            item_id TEXT NOT NULL,
+            time TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+            open_price FLOAT NOT NULL ,
+            close_price FLOAT NOT NULL ,
+            high_price FLOAT NOT NULL ,
+            low_price FLOAT NOT NULL ,
+            volume FLOAT NOT NULL ,
+            average_price FLOAT NOT NULL,
+            PRIMARY KEY (item_id, time),
+            CONSTRAINT fk_item FOREIGN KEY (item_id) REFERENCES items (item_id)
+        );
+    ")).await.expect("1 week chart");
+
+    pool.execute(sqlx::query("
+        CREATE TABLE IF NOT EXISTS one_month_chart(
+            item_id TEXT NOT NULL,
+            time TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+            open_price FLOAT NOT NULL ,
+            close_price FLOAT NOT NULL ,
+            high_price FLOAT NOT NULL ,
+            low_price FLOAT NOT NULL ,
+            volume FLOAT NOT NULL ,
+            average_price FLOAT NOT NULL,
+            PRIMARY KEY (item_id, time),
+            CONSTRAINT fk_item FOREIGN KEY (item_id) REFERENCES items (item_id)
+        );
+    ")).await.expect("1 month chart");
+
 
     pool.execute(sqlx::query("
         CREATE INDEX ON sell_summ (item_id,time DESC);;        
@@ -238,5 +482,31 @@ async fn make_new_time_thing(){
     pool.execute(sqlx::query("
         CREATE INDEX ON qick_info (item_id,time DESC);        
     ")).await.expect("Index making error info");
-    }
+
+    pool.execute(sqlx::query("
+        CREATE INDEX ON one_min_chart (item_id,time DESC);
+    ")).await.expect("Index making error info");
+    pool.execute(sqlx::query("
+        CREATE INDEX ON five_min_chart (item_id,time DESC);
+    ")).await.expect("Index making error info");
+    pool.execute(sqlx::query("
+        CREATE INDEX ON thirty_min_chart (item_id,time DESC);
+    ")).await.expect("Index making error info");
+    pool.execute(sqlx::query("
+        CREATE INDEX ON one_hour_chart (item_id,time DESC);
+    ")).await.expect("Index making error info");
+    pool.execute(sqlx::query("
+        CREATE INDEX ON four_hour_chart (item_id,time DESC);
+    ")).await.expect("Index making error info");
+    pool.execute(sqlx::query("
+        CREATE INDEX ON one_day_chart (item_id,time DESC);
+    ")).await.expect("Index making error info");
+    pool.execute(sqlx::query("
+        CREATE INDEX ON one_week_chart (item_id,time DESC);
+    ")).await.expect("Index making error info");
+    pool.execute(sqlx::query("
+        CREATE INDEX ON one_month_chart (item_id,time DESC);
+    ")).await.expect("Index making error info");
+
+}
 
